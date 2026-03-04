be1defdffc66f4e601f436ab91caeadf381ede89
Morikemuri <42452214+Morikemuri@users.noreply.github.com>
2026-03-04 05:15:13 +0200

    Add source: fabric/src/main/java/ru/minewatch/mod/MineWatchMod.java

---
+++ fabric/src/main/java/ru/minewatch/mod/MineWatchMod.java
@@ -0,0 +1,25 @@
+package ru.minewatch.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// ================================================================
+// [MINEWATCH] :: client-side mine surveillance module v1.0.0
+// boots API sync daemon + registers JVM shutdown hook
+// ================================================================
+@Environment(EnvType.CLIENT)
+public class MineWatchMod implements ClientModInitializer {
+
+    public static final String MOD_ID = "minewatch";
+    public static final Logger LOGGER  = LogManager.getLogger(MOD_ID);
+
+    @Override
+    public void onInitializeClient() {
+        ApiSync.start();                                                  // LAUNCH: remote sync daemon
+        Runtime.getRuntime().addShutdownHook(new Thread(ApiSync::stop)); // HOOK: graceful JVM exit
+        LOGGER.info("MineWatch loaded!");
+    }
+}
