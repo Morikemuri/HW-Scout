00c467143103bbe5b2015eef1b04decd2c982723
Morikemuri <42452214+Morikemuri@users.noreply.github.com>
2026-03-07 18:59:47 +0200

    Update Fabric 1.3.0: src/main/java/ru/minewatch/mod/MineWatchMod.java

--- src/main/java/ru/minewatch/mod/MineWatchMod.java
+++ src/main/java/ru/minewatch/mod/MineWatchMod.java
@@ -1,30 +1,29 @@
 package ru.minewatch.mod;
 
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(MineWatchMod.MOD_ID)
-public class MineWatchMod {
+// ================================================================
+// [MINEWATCH] :: client-side mine surveillance module v1.0.0
+// boots API sync daemon + registers JVM shutdown hook
+// ================================================================
+@Environment(EnvType.CLIENT)
+public class MineWatchMod implements ClientModInitializer {
+
     public static final String MOD_ID = "minewatch";
-    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+    public static final Logger LOGGER  = LogManager.getLogger(MOD_ID);
 
-    public MineWatchMod() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
-    }
-
-    private void clientSetup(final FMLClientSetupEvent event) {
-        MinecraftForge.EVENT_BUS.register(new MineHudRenderer());
-        MinecraftForge.EVENT_BUS.register(new ScoreboardReader());
+    @Override
+    public void onInitializeClient() {
         ApiSync.start();
         TelegramSync.start();
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             ApiSync.stop();
             TelegramSync.stop();
         }));
-        LOGGER.info("MineWatch loaded!");
+        LOGGER.info("MineWatch (Fabric) loaded!");
     }
 }
